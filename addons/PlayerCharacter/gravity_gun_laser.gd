10f36ff920b964b108034bc51e172d9871272860
SHADOW-BORN <rivmor65@kingshigh.school.nz>
2025-08-07 14:16:54 +1200

    About to get real

---
+++ addons/PlayerCharacter/gravity_gun_laser.gd
@@ -0,0 +1,20 @@
+extends RayCast3D
+
+@onready var beam_mesh = $BeamMesh
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var cast_point
+	force_raycast_update()
+	
+	if is_colliding():
+		cast_point = to_local(get_collision_point())
+		
+		beam_mesh.mesh.height = cast_point.y
+		beam_mesh.position.y = cast_point.y/2
+	#if Global.held_object != null:
+		#pass
